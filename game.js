4a766a137b43a91d48b879c41d755aaa85049506
Roberto Quesada <robertoquesadalizana@gmail.com>
2015-11-13 14:19:50 +0100

    Add game score and points to the questions

--- game.js
+++ game.js
@@ -3,15 +3,14 @@ var Question = require('./question.js');
 
 
 var questions = new Question();
-questions.add('¿En que año estamos?', '2015', 5);
-questions.add('¿Como llaman a Raul en casa?', 'pompe', 5);
-questions.add('¿De que color es el caballo blanco como la leche de Santiago?', 'blanco como la leche', 5);
+questions.add('¿En que año estamos?', '2015', 1);
+questions.add('¿Como llaman a Raul en casa?', 'pompe', 15);
+questions.add('¿De que color es el caballo blanco como la leche de Santiago?', 'blanco como la leche', 10);
 
 var quiz = new Quiz(questions.allQuestions);
 var currentQuestion = quiz.getQuestion();
 
 quiz.initialPrint();
-
 
 // Prompt
 process.stdin.on('data', function (data) {
@@ -19,17 +18,21 @@ process.stdin.on('data', function (data) {
 
   if (input == currentQuestion.answer) {
     quiz.next();
+    quiz.totalPoints += currentQuestion.point;
     currentQuestion = quiz.getQuestion();
+
     if (currentQuestion == undefined) {
-      console.log('\n\nEnhorabuena!!! Has contestado todas las preguntas\n\n');
+      console.log('\n\nEnhorabuena!!! Tu puntuación es ' + quiz.totalPoints + ' \n\n');
       process.exit();
     }
+
   } else {
+    quiz.totalPoints -= currentQuestion.point;
     console.log('\nRespuesta incorrecta\n');
+
   }
 
   quiz.questionPrint();
   
 
 });
-
